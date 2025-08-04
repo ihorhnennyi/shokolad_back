534067ef2587d1f51c74464127ec2710c8de011d
ihorhnennyi <ihorhnennyi@gmail.com>
2025-08-04 10:36:20 +0300

    feat: connected MongoDB and generated initial modules structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,25 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
+import { OptionsModule } from './options/options.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		MongooseModule.forRoot(process.env.MONGODB_URI || ''),
+		AuthModule,
+		UsersModule,
+		ProductsModule,
+		CategoriesModule,
+		OptionsModule,
+		OrdersModule,
+	],
 })
 export class AppModule {}
